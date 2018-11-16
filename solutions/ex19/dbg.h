da1788f1b0be7279d8ca7d0619d27b5655512a5c
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-11-16 17:13:17 +0100

    completed ex19

--- solutions/ex19/dbg.h
+++ solutions/ex19/dbg.h
@@ -16,11 +16,11 @@
 
 #define log_err(M, ...) fprintf(stderr,\
 		"[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
-		clear_errno(), ##__VA_ARGS__)
+		clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr,\
 		"[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
-		clear_errno(), ##__VA_ARGS__)
+		clean_errno(), ##__VA_ARGS__)
 
 #define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
 		__FILE__, __LINE__, ##__VA_ARGS__)
