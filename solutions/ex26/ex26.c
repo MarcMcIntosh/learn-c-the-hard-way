eea9700d690dfc4b952ac789de84d045f509469a
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-11-28 13:27:46 +0100

    Log find opens a file called .logfind

--- solutions/ex26/ex26.c
+++ solutions/ex26/ex26.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dbg.h"
-
 
 int main(int argc, char *argv[])
 {
-	check(argc > 2, "No string provided");
+	// check will print out message and goto error if first paremter is flase
+	check(argc > 1, "No string provided");
+
+	FILE *file = fopen(".logfind", "r");
+	check(file, "Failed to open file");
 	return 0;
 error:
 	return 1;
