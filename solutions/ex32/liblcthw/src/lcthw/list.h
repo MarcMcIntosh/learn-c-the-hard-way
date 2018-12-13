ab733681ca086eeebacf73458e4491771f1b3407
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-13 13:04:24 +0100

    Ex32 worksWq

--- solutions/ex32/liblcthw/src/lcthw/list.h
+++ solutions/ex32/liblcthw/src/lcthw/list.h
@@ -20,7 +20,7 @@ typedef struct List {
 List *List_create();
 void List_destroy(List * list);
 void List_clear(List * list);
-void List_clear_history(List * List);
+void List_clear_destroy(List * List);
 
 #define List_count(A) ((A)->count)
 #define List_first(A) ((A)->first != NULL ? (A)->first->value : NULL)
