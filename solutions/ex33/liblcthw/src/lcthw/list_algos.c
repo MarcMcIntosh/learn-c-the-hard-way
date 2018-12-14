f18631565d74d05c75740c1e2004d0482a72d2b2
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-14 14:47:43 +0100

    trying to get the sorting algorythm to pass

--- solutions/ex33/liblcthw/src/lcthw/list_algos.c
+++ solutions/ex33/liblcthw/src/lcthw/list_algos.c
@@ -1,18 +1,44 @@
 #include <lcthw/list_algos.h>
-#include "dbg.h"
+#include <dbg.h>
 
-size_t array_length(char *words[]) 
-{
-	size_t i = 0;
-	while(words[i++]);
-	return i;
-}
 
 int List_bubble_sort(List * list, List_compare compare_function)
-{
-	// what's the length of the array ?
-	size_t number_of_elements = list->count;
-	debug("Number of words given to bubble sort: %ld", number_of_elements);
+{	// what's the length of the array ?
+
+	int should_sort = 1;
+
+	while(should_sort) {
+
+		should_sort = 0;
+		
+		LIST_FOREACH(list, first, next, curr) {
+			if(curr->next != NULL) {
+				int should_swap = compare_function(curr->value, curr->next->value);
+				if(should_swap > 0) {
+					void *temp = curr->value;
+					curr->value = curr->next->value;
+					curr->next->value = temp;
+					// reset should_sort;
+					should_sort = 1;
+				}
+			}
+		}
+	}
 
 	return 0;
+	/*
+	
+	LIST_FOREACH(list, first, next, curr) {
+		if(curr->next != NULL) {
+			int should_swap = compare_function(curr->value, curr->next->value);
+			if(should_swap > 0) {
+				void *temp = curr->value;
+				curr->value = curr->next->value;
+				curr->next->value = temp;
+			}
+		}
+	}
+
+	return 0;
+	*/
 }
