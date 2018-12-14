0967831db676ba81022c44da3b6c30594b396871
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-14 15:26:11 +0100

    Fixed return type for merge sort, test now fail

--- solutions/ex33/liblcthw/src/lcthw/list_algos.c
+++ solutions/ex33/liblcthw/src/lcthw/list_algos.c
@@ -27,3 +27,7 @@ int List_bubble_sort(List * list, List_compare compare_function)
 
 	return 0;
 }
+
+List * List_merge_sort(List * list, List_compare compare_function) {
+
+}
--- solutions/ex33/liblcthw/tests/list_algos_tests.c
+++ solutions/ex33/liblcthw/tests/list_algos_tests.c
@@ -9,7 +9,8 @@ char *values[] = { "XXXX", "1234", "abcd", "xjvef", "NDSS" };
 
 void print_list(List * list) {
 	LIST_FOREACH(list, first, next, curr) {
-		printf("%s, ", curr->value);
+		// printf("%s, ", curr->value);
+		printf("%p, ", curr->value);
 	}
 	printf("\n");
 }
@@ -68,7 +69,7 @@ char *test_bubble_sort()
 
 	return NULL;
 }
-/*
+
 char *test_merge_sort()
 {
 	List *words = create_words();
@@ -77,24 +78,24 @@ char *test_merge_sort()
 	List *res = List_merge_sort(res, (List_compare) strcmp);
 	mu_assert(is_sorted(res), "Words are not sorted after merge sort.");
 	
-	List *res2 = List_merge_sort(res, (List_compare) strcmp);
-	mu_assert(is_sorted(res), "Should still be sorted after merge sort.");
+	// List *res2 = List_merge_sort(res, (List_compare) strcmp);
+	// mu_assert(is_sorted(res), "Should still be sorted after merge sort.");
 	
-	List_destroy(res2);
+	// List_destroy(res2);
 	List_destroy(res);
 	
 	List_destroy(words);
 
 	return NULL;
 }
-*/
+
 
 char *all_tests()
 {
 	mu_suite_start();
 	
 	mu_run_test(test_bubble_sort);
-	// mu_run_test(test_merge_sort);
+	mu_run_test(test_merge_sort);
 
 	return NULL;
 }
