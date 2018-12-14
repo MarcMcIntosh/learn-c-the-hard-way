646ee3014ceb1b0da81b3b2718680785ebf543ba
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-14 12:44:49 +0100

    Added List_merge_sort declatation

--- solutions/ex33/liblcthw/src/lcthw/list_algos.h
+++ solutions/ex33/liblcthw/src/lcthw/list_algos.h
@@ -3,8 +3,10 @@
 
 #include <lcthw/list.h>
 
-int List_bubble_sort(char *words[], (List_compare) compare_function);
+typedef int (*List_compare) (const char *a, const char *b);
 
+int List_bubble_sort(char *words[], (List_compare) compare_function);
+int List_merge_sort(char *words[], (List_compare) compare_function);
 
 #endif
 
