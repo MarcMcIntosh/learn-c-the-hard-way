49058b56cd8f14c59da5924eda8d082668c9e582
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-14 13:16:01 +0100

    sort function should take a List type

--- solutions/ex33/liblcthw/src/lcthw/list_algos.h
+++ solutions/ex33/liblcthw/src/lcthw/list_algos.h
@@ -5,8 +5,8 @@
 
 typedef int (*List_compare) (const char *a, const char *b);
 
-int List_bubble_sort(char *words[], List_compare compare_function);
-int List_merge_sort(char *words[], List_compare compare_function);
+int List_bubble_sort(List *list, List_compare compare_function);
+int List_merge_sort(List *list, List_compare compare_function);
 
 #endif
 
