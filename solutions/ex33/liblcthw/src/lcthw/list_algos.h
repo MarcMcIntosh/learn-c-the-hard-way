12286ccfa7b60b2947d3636ce6bf551da4435e6e
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-17 14:29:20 +0100

    Added List_merge to header

--- solutions/ex33/liblcthw/src/lcthw/list_algos.h
+++ solutions/ex33/liblcthw/src/lcthw/list_algos.h
@@ -6,6 +6,9 @@
 typedef int (*List_compare) (const char *a, const char *b);
 
 int List_bubble_sort(List *list, List_compare compare_function);
+
+List * List_merge(List * left, List * right, List_compare compare_function);
+
 List *List_merge_sort(List *list, List_compare compare_function);
 
 #endif
