c67d10ca118c8def369a6add0df87380f5071be2
Marc McIntosh <email@marcmcintosh.ninja>
2019-01-04 18:10:47 +0100

    missaligend address issues with merge sort.

--- solutions/dynamic-array-sorting-and-searching/src/mergesort.c
+++ solutions/dynamic-array-sorting-and-searching/src/mergesort.c
@@ -15,7 +15,7 @@ void merge(void *dest[], size_t start, size_t mid, size_t end, size_t size, int 
 		if(diffrence <= 0) {
 			size_t s = size;
 			do {
-				char * tmp = *(dest + left);
+				void * tmp = *(dest + left);
 				dest[left++] = *(dest + right);
 				dest[right++] = tmp;
 			} while (--s > 0);
