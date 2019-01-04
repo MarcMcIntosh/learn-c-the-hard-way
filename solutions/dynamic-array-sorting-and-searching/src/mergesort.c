8446ca29eefaa46e97e8545ab68b31e75a3322c3
Marc McIntosh <email@marcmcintosh.ninja>
2019-01-04 17:55:42 +0100

    Can not get merge sort to work with darray,

--- solutions/dynamic-array-sorting-and-searching/src/mergesort.c
+++ solutions/dynamic-array-sorting-and-searching/src/mergesort.c
@@ -1,5 +1,54 @@
+#include <stdlib.h>
+#include <mergesort.h>
+#include <dbg.h>
+#include <darray.h>
 
-int mergesort (void *contents, size_t count, size_t size, int (*cmp) (const void *, const void * ));
+
+void merge(void *dest[], size_t start, size_t mid, size_t end, size_t size, int (*cmp) (const void *, const void *))
 {
+	size_t left = start;
+	size_t right = mid + 1;
+	
+	while(left < mid || right < end) {
+		int diffrence = cmp(dest[left], dest[right]);
 
+		if(diffrence <= 0) {
+			size_t s = size;
+			do {
+				char tmp = *dest[left];
+				*dest[left++] = *dest[right];
+				*dest[right++] = tmp;
+			} while (--s > 0);
+		}
+		
+	}
+}
+
+void split(void *dest, size_t start, size_t end, size_t size, int (*cmp) (const void *, const void *))
+{
+	if(start >= end) { return; }
+
+	int mid = (start + (end - start)) / 2;
+
+	// left
+	
+	split(dest, start, mid, size, cmp);
+	
+	// right
+	split(dest, mid + 1, end, size, cmp);
+
+	merge(dest, start, mid, end, size, cmp);
+}
+
+// sort cmp is strcmp
+int mergesort (void *contents, size_t count, size_t size, int (*cmp) (const void *, const void * ))
+{
+	// https://codereview.stackexchange.com/questions/131407/mergesort-algorithm-in-c
+	// https://www.geeksforgeeks.org/merge-sort/
+
+	if(count <= 1) { return 0; }
+
+	split(contents, 0, count, size, cmp);
+
+	return 0;
 }
