b907832d4d0add169da3b6289f57b75ad7aba1e3
Marc McIntosh <marcmcintsoh1987@gmail.com>
2019-04-14 16:47:39 +0200

    Correct errors that are in the book

--- solutions/ring-buffer/src/ringbuffer.h
+++ solutions/ring-buffer/src/ringbuffer.h
@@ -4,10 +4,10 @@
 #include <bstrlib.h>
 
 typedef struct {
-	char *buffer;
-	int length;
-	int start;
-	int end;
+    char *buffer;
+    int length;
+    int start;
+    int end;
 } RingBuffer;
 
 RingBuffer *RingBuffer_create(int length);
@@ -29,34 +29,34 @@ int RingBuffer_available_space(RingBuffer * buffer);
 bstring RingBuffer_gets(RingBuffer * buffer, int amount);
 
 #define RingBuffer_available_data(B) (\
-		((B)->end + 1) % (B)->length - (B)->start -1)
+        (B)->end % (B)->length - (B)->start)
 
 #define RingBuffer_available_space(B) (\
-		(B)->length - (B)->end - 1)
+        (B)->length - (B)->end - 1)
 
-#define RingBuffer_full(B) (\
-		RingBuffer_available_data((B)) - (B)->length == 0)
+#define RingBuffer_full(B) (RingBuffer_available_space(B) == 0)
 
-#define RingBuffer_empty(B) (\
-		RingBuffer_available_data(B) == 0)
+#define RingBuffer_empty(B) (RingBuffer_available_data((B)) == 0)
 
 #define RingBuffer_puts(B, D) RingBuffer_write(\
-		(B), bdata((D)), blength((D)))
+        (B), bdata((D)), blength((D)))
 
 #define RingBuffer_get_all(B) RingBuffer_gets(\
-		(B), RingBuffer_available_data((B)))
+        (B), RingBuffer_available_data((B)))
 
 #define RingBuffer_starts_at(B) (\
-		(B)->buffer + (B)->start)
+        (B)->buffer + (B)->start)
 
 #define RingBuffer_ends_at(B) (\
-		(B)->buffer + (B)->end)
+        (B)->buffer + (B)->end)
 
 #define RingBuffer_commit_read(B, A) (\
-		(B)->start = ((B)->end + (A)) % (B)->length)
+        (B)->start = ((B)->start + (A)) % (B)->length)
 
 #define RingBuffer_commit_write(B, A) (\
-		(B)->end = ((B)->end + (A)) % (B)->length)
+        (B)->end = ((B)->end + (A)) % (B)->length)
+
+#define RingBuffer_clear(B) RingBuffer_commit_read((B),\
+        RingBuffer_available_data((B)));
 
 #endif
-
